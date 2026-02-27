f0433d8a0771820d15265a072535fa964635d30d
Andy wu <lpageo@163.com>
2026-02-28 02:20:53 +0800

    refactor(preload): move preload to src/preload

--- src/main/window.js
+++ src/main/window.js
@@ -11,7 +11,7 @@ function createWindow() {
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false,
-      preload: path.join(__dirname, '../../preload.js'),
+      preload: path.join(__dirname, '../preload/index.js'),
     },
   });
 
