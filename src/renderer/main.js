5610317e9f671b0bb270435652cccaa3b4429ed7
wujiahe <wujiahe@zhuanzhuan.com>
2026-02-28 16:05:55 +0800

    style(renderer): make terminal full-bleed

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import './terminal-fullbleed.css';
 import { state } from './state.js';
 import { copyTextToClipboard, normalizeProject } from './utils.js';
 import { initStaticIcons } from './icons.js';
