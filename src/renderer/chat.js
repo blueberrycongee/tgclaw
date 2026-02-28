7096f018bf4d7030e5f30f6328e6308f32ba1619
Andy wu <lpageo@163.com>
2026-02-28 12:12:42 +0800

    perf(chat): optimize highlight.js bundle with selective language imports

--- src/renderer/chat.js
+++ src/renderer/chat.js
@@ -1,10 +1,47 @@
 import { Marked } from 'marked';
 import { markedHighlight } from 'marked-highlight';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import python from 'highlight.js/lib/languages/python';
+import bash from 'highlight.js/lib/languages/bash';
+import json from 'highlight.js/lib/languages/json';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import rust from 'highlight.js/lib/languages/rust';
+import go from 'highlight.js/lib/languages/go';
+import java from 'highlight.js/lib/languages/java';
+import c from 'highlight.js/lib/languages/c';
+import cpp from 'highlight.js/lib/languages/cpp';
+import sql from 'highlight.js/lib/languages/sql';
+import yaml from 'highlight.js/lib/languages/yaml';
+import markdown from 'highlight.js/lib/languages/markdown';
+import diff from 'highlight.js/lib/languages/diff';
+import shell from 'highlight.js/lib/languages/shell';
 import 'highlight.js/styles/github-dark.css';
 import { state } from './state.js';
 import { gateway } from './gateway.js';
 import { renderSessions } from './sidebar.js';
+
+[
+  ['javascript', javascript],
+  ['typescript', typescript],
+  ['python', python],
+  ['bash', bash],
+  ['json', json],
+  ['xml', xml],
+  ['css', css],
+  ['rust', rust],
+  ['go', go],
+  ['java', java],
+  ['c', c],
+  ['cpp', cpp],
+  ['sql', sql],
+  ['yaml', yaml],
+  ['markdown', markdown],
+  ['diff', diff],
+  ['shell', shell],
+].forEach(([name, language]) => hljs.registerLanguage(name, language));
 const marked = new Marked(markedHighlight({
   emptyLangClass: 'hljs',
   langPrefix: 'hljs language-',
